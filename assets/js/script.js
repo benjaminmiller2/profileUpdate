4d9dbba9ccc8dd68fa935734570223466b16300e
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-06-13 16:37:34 -0500

    fourth

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,27 @@ $(document).ready(function(){
     $(window).scroll(function(){
 
         let header = $(".header");
+        let container = $(".container");
         let scroll = $(window).scrollTop();
         let offset = $(".js-header").offset();
 
         if (scroll >= offset.top){
             header.addClass("sticky");
+            container.addClass("padding");
         }else{
-            header.removeClass("sticky")
+            header.removeClass("sticky");
+            container.removeClass("padding")
         }
     });
+    $(".js-nav-button").mouseenter(function(){
 
+        $(".dropdown-content").css("display","block")
+    
+    });
+
+    $(".dropdown-content").mouseleave(function(){
+        $(".dropdown-content").css("display","none")
+    })
 
 });
 
